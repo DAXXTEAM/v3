8679210a56caebdacdb12287548c00e8dd13e09e
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-03-14 16:06:28 +0530

    Update main.py

--- registerlogin/main.py
+++ registerlogin/main.py
@@ -12,7 +12,7 @@ jwt = JWTManager(app)
 # can confirm this is random, gotten with a fair rolled dice
 app.config["JWT_SECRET_KEY"] = '#^\x04<\x90\tH^\x83\x05\xa2\x88\xfe8s\xad\x9e_\xd6\x82I\xed\xe4\xdf\xb9\x92\x80\xcc\x8d:\xf0\xe7\xb3|\x16Ssy\xd4\x01\x0b"\x0e;nc\xb1\xbb\xd0\xe1\xd0\\@\x11e\xa3\xbb\xb3\x1b\x83\x99\xde\x8d}'
 
-client = MongoClient('mongodb://mohio:ck1QpFfooZ3Nb30k@185.253.54.63:27017/admin?authSource=admin')
+client = MongoClient('mongodb+srv://iamdaxx404:asd@mohio.1uwb6r5.mongodb.net')
 db = client.admin
 collection = db.mohio
 paymentkeys = db.paymentkeys
